fb6ce2a4720cc8e62b9d437748ca5bd0bf162d64
Felix Zumstein <fzumstein@gmail.com>
2017-10-25 16:41:39 +0200

    added mean reverting price process

--- simulation.py
+++ simulation.py
@@ -14,6 +14,8 @@ def simulate(num_timesteps, num_simulations, starting_price, mu, vol, dt, perc_s
     for t in range(1, num_timesteps + 1):
         rand_nums = np.random.randn(num_simulations)
         price[t, :] = price[t - 1, :] * np.exp((mu - 0.5 * vol ** 2) * dt + vol * rand_nums * np.sqrt(dt))
+        # price[t, :] = price[t - 1, :] * np.exp((mu * (np.log(starting_price) - np.log(price[t - 1, :])) -
+        #                                         0.5 * vol ** 2) * dt + vol * rand_nums * np.sqrt(dt))
         percentiles[t, :] = np.percentile(price[t, :], perc_selection)
 
     # Return percentiles and sample path
